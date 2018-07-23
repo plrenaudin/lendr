39f7770f0b970102d27d2fd12dd11413bfd4e499
plrenaudin <pl.renaudin@gmail.com>
2018-07-22 22:04:47 -0400

    add tabs and start inventory

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,44 @@
 <Card className="content">
-  <AddItemForm />
+  <nav>
+    <ul>
+      <li on:click="set({tab:'home'})">Home</li>
+      <li on:click="set({tab:'inventory'})">Inventory</li>
+    </ul>
+  </nav>
+  {#if tab==="home"}
+    <Home />
+  {:elseif tab === "inventory"}
+    <Inventory />
+  {/if}
 </Card>
 
 <script>
   export default {
+    data() {
+      return {
+        tab: "home"
+      };
+    },
     components: {
-      AddItemForm: "./components/AddItemForm",
-      Card: "./components/Card"
+      Card: "./components/Card",
+      Home: "./Home",
+      Inventory: "./Inventory"
     }
   };
 </script>
 
 <style>
   @import "./styles";
+  nav ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: space-around;
+  }
+  nav ul li {
+    font-weight: bold;
+    width: 100%;
+    cursor: pointer;
+    text-align: center;
+    color: var(--linkColor);
+  }
 </style>
---
+++ src/Inventory.svelte
@@ -0,0 +1,22 @@
+<main>
+  <h1>This is the Inventory</h1>
+  <AddItemForm />
+  <ul>
+  {#each $items as item}
+    <li>{item.id} {item.description}</li>
+  {/each}
+  </ul>
+</main>
+
+<script>
+  export default {
+    data() {
+      return {
+        tab: "home"
+      };
+    },
+    components: {
+      AddItemForm: "./components/AddItemForm"
+    }
+  };
+</script>
