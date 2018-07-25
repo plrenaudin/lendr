8155c922e99e36348c832efbaf1793d3e20d0261
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-25 02:11:24 +0000

    Deploy plrenaudin/lendr to github.com/plrenaudin/lendr.git:gh-pages

--- src/components/AddItemForm/index.svelte
+++ src/components/AddItemForm/index.svelte
@@ -3,8 +3,10 @@
     Item S/N
     <input bind:value=id type="text" />
     <ScanButton on:scanResult="set({id: event.data})" />
-    <input type="text" bind:value=description />
-    <Button on:click="addItem()" bind:disabled>Add</Button>
+    {#if id}
+      <input type="text" bind:value=description />
+      <Button on:click="addItem()" bind:disabled>Add</Button>
+    {/if}
   </label>
 </section>
 
@@ -25,11 +27,17 @@
     },
     methods: {
       addItem() {
-        this.store.set({
-          items: this.store.get().items.concat({ id: this.get().id, description: this.get().description })
-        });
+        this.store.addItem({ id: this.get().id, description: this.get().description });
         this.set({ id: "", description: "" });
       }
+    },
+    oncreate() {
+      this.on("state", ({ changed, current }) => {
+        if (changed.id) {
+          const found = this.store.get().items.find(i => i.id === current.id);
+          found && this.set({ description: found.description });
+        }
+      });
     }
   };
 </script>
