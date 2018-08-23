05a794e51c2ad9487484408f20d248f470b1779c
plrenaudin <pl.renaudin@gmail.com>
2018-08-22 21:35:13 -0400

    add test on menu

--- test/integration/emptyPath_spec.js
+++ test/integration/emptyPath_spec.js
@@ -12,6 +12,11 @@ describe("Lendr without data works", () => {
       .click();
     cy.get(".inventory-container").should("be.visible");
 
+    cy.get(".menu-container button").click();
+    cy.get(".menu li").contains(t("importExport"));
+    cy.get(".overlay").click();
+    cy.get(".menu li").should("not.exist");
+
     cy.get("table tbody tr td").contains(t("inventory.noResults"));
     cy.get("nav li")
       .contains(t("inventory.tab.loans"))
@@ -21,11 +26,13 @@ describe("Lendr without data works", () => {
     cy.get(".inventory-container").should("not.be.visible");
   });
 
+  it("Opens the menu and closes it", () => {});
+
   xit("Uses the scan features", () => {
-    cy.get("#scanner").should("not.exists");
+    cy.get("#scanner").should("not.exist");
     cy.get(".scan-button-container button").click();
     cy.get("#scanner").should("be.visible");
     cy.get(".overlay").click();
-    cy.get("#scanner").should("not.exists");
+    cy.get("#scanner").should("not.exist");
   });
 });
