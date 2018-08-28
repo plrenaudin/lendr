96883699945a33cccf8a77c903af50d7de653106
plrenaudin <pl.renaudin@gmail.com>
2018-08-27 22:13:50 -0400

    Add name completion for loaners

--- src/components/LendForm/index.svelte
+++ src/components/LendForm/index.svelte
@@ -1,12 +1,22 @@
 <section class="lend-form">
   <label>
       <input type="text" bind:value=name placeholder="Name" ref:input />
+      {#if suggestions.length}
+        <div class="overlay" on:click="set({name:''})"></div>
+        <ul class="suggestions" style="transform: translateY({suggestionsTranslate}rem)">
+          {#each suggestions as suggestion}
+            <li on:click="select(suggestion)">{suggestion}</li>
+            {/each}
+        </ul>
+      {/if}
       <Button on:click="lendItem()" bind:disabled icon="upload">{t("action.lendAction")}</Button>
   </label>
 </section>
 
 <script>
   import t from "../../utils/wording";
+  import { includes } from "../../utils/search";
+
   export default {
     components: {
       Button: "../Button"
@@ -17,13 +27,19 @@
       };
     },
     computed: {
-      disabled: ({ id, name }) => !id || !name
+      disabled: ({ id, name }) => !id || !name,
+      suggestions: ({ $loaners, name }) =>
+        name.length && $loaners.filter(i => includes(name, i) && i !== name).slice(0, 8),
+      suggestionsTranslate: ({ suggestions }) => -2.1 * Math.min(5, suggestions.length) - 3.1
     },
     methods: {
       lendItem() {
         this.store.lendItem({ id: this.get().id, name: this.get().name });
         this.set({ id: "", name: "" });
         this.fire("lent");
+      },
+      select(name) {
+        this.set({ name });
       }
     },
     helpers: {
@@ -34,3 +50,31 @@
     }
   };
 </script>
+
+<style>
+  @import "../../styles/variables.css";
+
+  label {
+    position: relative;
+  }
+  .overlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
+  ul.suggestions {
+    width: 100%;
+    list-style-type: none;
+    position: absolute;
+    background-color: var(--fgColor);
+    box-shadow: 0 1px 3px var(--shadowColor);
+    max-height: 10.5rem;
+    overflow: auto;
+    z-index: 3;
+  }
+  ul.suggestions li {
+    padding: 0.3rem 0.5rem;
+  }
+</style>
